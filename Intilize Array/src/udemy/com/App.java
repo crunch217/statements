c1b28a34c265ca0ea69c305fdb0a75e3b8b65aa8
lenovo <lenovo@LAPTOP-9SR0RTJ8>
2022-02-14 15:08:46 +0530

    SecondCommit

---
+++ Intilize Array/src/udemy/com/App.java
@@ -0,0 +1,18 @@
+package udemy.com;
+
+public class App {
+
+	public static void main(String[] args) {
+//		int[] intVariableArray = {1,2,3,4,5,6,7,8,9,10};
+//		System.out.println(intVariableArray[5]);
+//		float[] floatVariableArray = {0.0f,1.0f,2.0f,3.0f,4.0f};	
+//		System.out.println(floatVariableArray[4]);
+		
+String[] stringArray = {"NTR","Rupali","Mia","Salim"};
+		
+		for(String name: stringArray){
+			System.out.println(name);
+	}
+
+	}
+}
